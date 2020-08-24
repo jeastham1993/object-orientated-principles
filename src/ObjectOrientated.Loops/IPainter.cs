c5a426cbcea68e94ccc021a099fd591e6e21007f
James Eastham <j.eastham1993@gmail.com>
2020-08-24 20:42:10 +0100

    [FEATURE] Add looping examples

---
+++ src/ObjectOrientated.Loops/IPainter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientated.Loops
+{
+    interface IPainter
+    {
+        bool IsAvailable { get; set; }
+
+        TimeSpan EstimateTimeToPaint(
+	        double sqMetres);
+
+        double EstimateCompensation(
+	        double sqMetres);
+    }
+}
---
+++ src/ObjectOrientated.Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectOrientated.Loops
+{
+    class Program
+    {
+	    private static IPainter FindCheapestPainterWithLoop(
+		    double sqMetres,
+		    IEnumerable<IPainter> painters)
+	    {
+		    var bestPrice = 0D;
+		    IPainter cheapest = null;
+
+		    foreach (var painter in painters)
+		    {
+			    if (painter.IsAvailable)
+			    {
+				    var price = painter.EstimateCompensation(sqMetres);
+
+				    if ( cheapest == null || price < bestPrice)
+				    {
+					    cheapest = painter;
+				    }
+			    }
+		    }
+
+		    return cheapest;
+	    }
+
+	    private static IPainter FindCheapestPainter(
+		    double sqMetres,
+		    IEnumerable<IPainter> painters)
+	    {
+		    return painters
+			    .Where(p => p.IsAvailable)
+			    .WithMinimum(painter => painter.EstimateCompensation(sqMetres));
+	    }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
